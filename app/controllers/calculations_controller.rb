27bf1218574fefb753145c3f42838b2003b69d1f
Derrick <wolbert.derrick@gmail.com>
2015-05-10 14:26:41 -0500

    done
    done

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -3,8 +3,24 @@ class CalculationsController < ApplicationController
   end
 
   def sqrt
+    @number = params["number"].to_i
+    @sqrt = Math.sqrt(@number)
   end
 
   def pmt
+    @basis = params["basis"].to_f
+    @periods = params["periods"].to_f
+    @principal = params["principal"].to_f
+
+    rate = @basis/10000
+    nper = @periods
+
+    @pmt = -(rate * @principal)/(1 - (1 + rate)**-nper)
+
+  end
+
+  def square
+    @number = params["number"].to_i
+    @square = @number**2
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "calculations", :action => "instructions" })
+  get("/",                { :controller => "calculations", :action => "instructions" })
+  get("/square/:number",  { :controller => "calculations", :action => "square" })
+  get("/square_root/:number",  { :controller => "calculations", :action => "sqrt" })
+  get("/payment/:basis/:periods/:principal",  { :controller => "calculations", :action => "pmt" })
 end
